389772559ba36551a6d9557f06ff9ba17d22a711
Marshall <mblgh6@mail.missouri.edu>
2018-09-21 13:21:22 -0500

    Added the expected files for the project.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+
+Author: Marshall Lindsay
+Email: mblgh6@mail.missouri.edu
+Class Code: CS4770
+Project Name: Lab1PartA
+
+*/
+#include "neuralNetwork.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv){
+
+  return(0);
+}
---
+++ neurons.cpp
@@ -0,0 +1,8 @@
+/*
+
+Author: Marshall Lindsay
+Email: mblgh6@mail.missouri.edu
+Class Code: CS4770
+Project Name: Lab1PartA
+
+*/
