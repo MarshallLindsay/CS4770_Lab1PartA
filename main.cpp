083377f2314aa96f53f91ca173cfc68a05d0b825
Marshall <mblgh6@mail.missouri.edu>
2018-09-21 13:45:39 -0500

    Starting to fill out neurons.h. Also confirmed main.cpp with helloworld.

--- main.cpp
+++ main.cpp
@@ -11,8 +11,9 @@ Project Name: Lab1PartA
 #include <stdio.h>
 #include <stdlib.h>
 
+using namespace std;
 
 int main(int argc, char **argv){
-
+  cout << "Hello World!" << endl;
   return(0);
 }
--- neurons.h
+++ neurons.h
@@ -6,3 +6,32 @@ Class Code: CS4770
 Project Name: Lab1PartA
 
 */
+
+#ifndef NEURONS_H_
+#define NEURONS_H_
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector.h>
+
+/*
+Brief: The rosenblattPerceptron class is used to describe the features
+associated with, you guess it, the rosenblatt perceptron. We rename the
+class to "Neuron" as this will be the only
+*/
+
+class rosenblattPerceptron{
+private:
+  int8_t location[2];   // layer , row.
+  double localGradient;
+  double localField;
+  double output;
+
+public:
+
+
+} Neuron;
+
+
+#endif // NEURONS_H_
