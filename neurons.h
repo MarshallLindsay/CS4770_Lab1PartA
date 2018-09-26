a82d17cb9f3ea8ea9bfa3e69bdfebee6883da3d7
Marshall <mblgh6@mail.missouri.edu>
2018-09-25 20:56:14 -0500

    Continuing to finish the neuron.h

--- neurons.h
+++ neurons.h
@@ -18,20 +18,43 @@ Project Name: Lab1PartA
 /*
 Brief: The rosenblattPerceptron class is used to describe the features
 associated with, you guess it, the rosenblatt perceptron. We rename the
-class to "Neuron" as this will be the only
+class to "Neuron" as this will be the only neuron type.
 */
 
 class rosenblattPerceptron{
 private:
-  int8_t location[2];   // layer , row.
-  double localGradient;
-  double localField;
-  double output;
+  int8_t location[2];       //layer , number.
+  double localGradient;     //delta from notes
+  double localField;        //v_j(k) from notes
+  double output;            //y_j(k) from notes
+  vector<double> weights;   //Vector of all of the weights
+
+  void initializeWeights(); //Initialize the weight vector with random values
 
 public:
+  Neuron();                 //Initialize the neuron
+  int8_t getLayer();        //Return the layer that the neuron is in
+  int8_t getNumber();       //Return the neuron's number in the layer
+  double getLocalGradient();//Return the local gradient
+  double getLocalField();   //Return the local field
+  double getOutput();       //Return the output of the neuron
+  vector<double> getWeights();//Return the weight vector
+  double getWeight(int number);//Return a single specified weight
 
 
 } Neuron;
 
+
+class network {
+private:
+  vector<int> inputs;       //Input vector
+  vector<int> outputs;      //Output vector
+  int numberOfLayers;       //Number of layers in the network
+  vector<int> neuronsInLayer;//Vector that contains the number of neurons in a layer
+  vector<Neuron> neurons;   //Vector of al the neurons in the network
+
+public:
+
+};
 
 #endif // NEURONS_H_
