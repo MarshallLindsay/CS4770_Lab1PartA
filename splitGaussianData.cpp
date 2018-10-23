da3360c00fdd6824a053e422b7452965f52c8b7f
Marshall <mblgh6@mail.missouri.edu>
2018-10-22 22:32:23 -0500

    Added more plots and more to splitting the gaussian data.

--- splitGaussianData.cpp
+++ splitGaussianData.cpp
@@ -10,23 +10,42 @@
 using namespace std;
 void readData(string filename);
 void printData(vector<vector<double> > data);
+void splitData(string firstClassFileName, string secondClassFileName);
+void saveData(string filename, vector<vector<double>> data);
 
 vector<vector<double> > inputData;
+vector<vector<double>> classOneInputData;
+vector<vector<double>> classTwoInputData;
 
 int main(int argc, char** argv){
 
   string class1file = "gausclass1.dat";
   string class2file = "gausclass2.dat";
-  string firsttwenty = "firsttwenty.dat";
-  string secondtwenty = "secondtwenty.dat";
-  string thirdtwenty = "thirdtwenty.dat";
-  string fourthtwenty = "fourthtwenty.dat";
-  string fithtwenty = "fithtwenty.dat";
+  string firsttwenty = "firsttraining.dat";
+  string secondtwenty = "secondTraining.dat";
+  string thirdtwenty = "thirdTraining.dat";
+  string fourthtwenty = "fourthTraining.dat";
+  string fithtwenty = "fithTraining.dat";
+  string firsttraining = "firstTesting.dat";
+  string secondtraining = "secondTesting.dat";
+  string thirdtraining = "thirdTesting.dat";
+  string fourthtraining = "fourthTesting.dat";
+  string fifthtraining = "fifthTestin.dat";
 
   string inputFile = "Two_Class_FourDGaussians500.txt";
 
   readData(inputFile);
   printData(inputData);
+  splitData(class1file, class2file);
+  printData(classTwoInputData);
+  printData(classOneInputData);
+  saveData(class1file, classOneInputData);
+  saveData(class2file, classTwoInputData);
+
+
+
+
+
 
 }
 
@@ -45,7 +64,8 @@ void readData(string filename){
     stringstream check1(line);
     string intermediate;
     vector<double> data;
-    while(getline(check1, intermediate, " ")){
+    while(getline(check1, intermediate, ' ')){
+      if(!atof(intermediate.c_str()) == 0)
       data.push_back(atof(intermediate.c_str()));
     }
     inputData.push_back(data);
@@ -63,6 +83,72 @@ void printData(vector<vector<double> > data){
   }
 }
 
+void splitData(string firstClassFileName, string secondClassFileName){
+  ofstream outputfile;
+  outputfile.open(firstClassFileName);
+  if(!outputfile.is_open()){
+    cout<<"Could not open "<<firstClassFileName<<endl;
+    exit(1);
+  }
+
+  ofstream anotherOutputFile;
+  anotherOutputFile.open(secondClassFileName);
+  if(!anotherOutputFile.is_open()){
+    cout<<"Could not open "<<secondClassFileName<<endl;
+    exit(1);
+  }
+  vector<double> tempVector;
+  for(int i = 0; i < inputData.size(); i++){
+    for(int j = 0; j < inputData[i].size(); j++){
+      tempVector.push_back(inputData[i][j]);
+    }
+    if(tempVector.back() == 1){
+      classOneInputData.push_back(tempVector);
+    }else{
+      classTwoInputData.push_back(tempVector);
+    }
+    tempVector.clear();
+  }
+
+  outputfile.close();
+  anotherOutputFile.close();
+}
+
+void saveData(string filename, vector<vector<double>> data){
+
+  ofstream outputFile;
+  outputFile.open(filename);
+  if(!outputFile.is_open()){
+    cout<<"Could not open "<<filename<<endl;
+    exit(1);
+  }
+
+  for(int i = 0; i < data.size(); i++){
+    for(int j = 0; j < data[i].size(); j++){
+      outputFile << data[i][j] << " ";
+    }
+    outputFile<<endl;
+  }
+
+}
+void saveTestResults(string filename, vector<double> inputData, vector<double>outputData){
+  ofstream outputFile;
+  outputFile.open(filename, ios_base::app);
+  if(!outputFile.is_open()){
+    cout<<"Could not open the output file " << filename << endl;
+    exit(1);
+  }
+  double label;
+  if(outputData[0] > outputData[1]){
+    label = 0;
+  }else{
+    label = 1;
+  }
+
+  outputFile << inputData[0] << " " << inputData[1] << " " << label <<endl;
+
+  outputFile.close();
+}
 
 /*
 void readWeights(string filename){
